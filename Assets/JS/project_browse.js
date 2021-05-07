f5981e052d02d57c9cf67fa48e71e836b2a9a5e6
Dina Razek <drazek@andrew.cmu.edu>
2021-05-07 18:51:39 -0400

    Added expansion of photo on project detail page

--- Assets/JS/project_browse.js
+++ Assets/JS/project_browse.js
@@ -1,6 +1,6 @@
 function pulse(id){
     var carousel_item = document.getElementById(id);
-    carousel_item.setAttribute('class', 'pulse');
+    carousel_item.setAttribute('class', 'pulseDetail');
 }
 
 // on hover, pulse for time and then remove attribute
@@ -10,5 +10,6 @@ function hoverPulse(id) {
     carousel_item.setAttribute('class', 'pulse');
     setTimeout(pulse, 3000, id);
     carousel_item.removeAttribute('class');
+    carousel_item.setAttribute('class', 'img-fluid');
 
 }
