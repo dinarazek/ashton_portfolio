5b23faecd0f27ce8a4aa5f9cc4ef902f40827dbf
Dina Razek <drazek@andrew.cmu.edu>
2021-05-07 12:12:00 -0400

    Added reflection, comments, and pulse animation to project browse page

---
+++ Assets/JS/project_browse.js
@@ -0,0 +1,14 @@
+function pulse(id){
+    var carousel_item = document.getElementById(id);
+    carousel_item.setAttribute('class', 'pulse');
+}
+
+// on hover, pulse for time and then remove attribute
+// move id to different element to do
+function hoverPulse(id) {
+    var carousel_item = document.getElementById(id);
+    carousel_item.setAttribute('class', 'pulse');
+    setTimeout(pulse, 3000, id);
+    carousel_item.removeAttribute('class');
+
+}
