afb7d6664c068dbbf33ece6f9fb509dd4fa9578f
Dina Razek <drazek@andrew.cmu.edu>
2021-05-07 23:29:01 -0400

    Added alt text to all images, added comments to HTML, CSS, and JS files

--- Assets/JS/project_browse.js
+++ Assets/JS/project_browse.js
@@ -1,15 +1,6 @@
+// used to pulse image on project detail page on mouse down
+// paired with expansion to give interactiv effect when opening a project detail page
 function pulse(id){
     var carousel_item = document.getElementById(id);
     carousel_item.setAttribute('class', 'pulseDetail');
-}
-
-// on hover, pulse for time and then remove attribute
-// move id to different element to do
-function hoverPulse(id) {
-    var carousel_item = document.getElementById(id);
-    carousel_item.setAttribute('class', 'pulse');
-    setTimeout(pulse, 3000, id);
-    carousel_item.removeAttribute('class');
-    carousel_item.setAttribute('class', 'img-fluid');
-
 }
